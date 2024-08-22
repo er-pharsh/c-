d8fa2c69d3f5a89d63b8954e47650c350900d98b
Harsh <er.pharsh@gmail.com>
2024-08-22 20:05:40 +0530

    Add new code

--- if-else.cpp
+++ if-else.cpp
@@ -2,14 +2,32 @@
 using namespace std;
 
 int main(){
- int num;
- cout << "Enter the positive number :- ";
- cin >> num;
- if(num % 2 == 0){
-  cout << "Integer is Even.";
+ // int num;
+ // cout << "Enter the positive number :- ";
+ // cin >> num;
+ // if(num % 2 == 0){
+ //  cout << "Integer is Even.";
+ // }
+ // else{
+ //  cout << "Integer is Odd.";
+ // }
+
+ int l;
+ cout << "Enter the length of rectangle :- ";
+ cin >> l;
+
+ int b;
+ cout << "Enter the breadth of rectangle :- ";
+ cin >> b;
+
+ int area = l*b;
+ int perimeter = 2*(l+b);
+
+ if (area > perimeter){
+  cout << "Area is greater than perimeter.";
  }
  else{
-  cout << "Integer is Odd.";
+  cout << "Primeter is greater than area.";
  }
 
  return 0;
