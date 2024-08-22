c280d0b2d555938ff9fb1cc590202da3f9ad5fce
Harsh <er.pharsh@gmail.com>
2024-08-22 18:37:06 +0530

    If-Else statement

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int num;
+ cin >> num;
+ if(num % 2 == 0){
+  cout << "Integer is Even.";
+ }
+ else{
+  cout << "Integer is Odd.";
+ }
+
+ return 0;
+}
