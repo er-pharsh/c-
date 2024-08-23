ac483d85746b34163ae7c71268066a21ce9745f1
Harsh <er.pharsh@gmail.com>
2024-08-23 20:32:47 +0530

    Add new code

--- loop.cpp
+++ loop.cpp
@@ -6,9 +6,17 @@ int main(){
  //  cout << i << " Hello World" << endl ;
  // }
 
- for (int i=1 ; i<=100 ; i=i+2){
-  cout << i << " ";
+ // for (int i=1 ; i<=100 ; i=i+2){
+ //  cout << i << " ";
+ // }
+
+ int n;
+ cout << "Enter the number :- ";
+ cin >> n;
+
+ for (int i=1 ; i<=10 ; i++){
+  cout << n << " * " << i << " = " << n*i << endl;  
  }
- 
+
  return 0;
 }
