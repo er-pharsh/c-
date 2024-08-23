1c5b54a498baf1067cfaa586d9d84ba563f96f92
Harsh <er.pharsh@gmail.com>
2024-08-23 21:44:09 +0530

    Add new code

--- loop.cpp
+++ loop.cpp
@@ -33,16 +33,63 @@ int main(){
 
  // GP 1 2 4 8 16 32.......
 
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // int a = 1;
+
+ // for (int i=1 ; i<=n ; i++){
+ //  cout << a << " ";
+ //  a = a * 2;
+ // } 
+
+ // WAP find the highest fector of the given number
+
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // int a;
+
+ // // for (int i=1 ; i<n/2 ; i++){
+ // //  if (n%i == 0){
+ // //   a = i;
+ // //  }
+ // // }
+
+ // for (int i=n/2 ; i>=1 ; i--){
+ //  if (n%i == 0){
+ //   a = i;
+ //   break;
+ //  }
+ // }
+ // cout << "The highest fector is :- " << a ;
+
+ // Number is prime or composite
+
  int n;
  cout << "Enter the number :- ";
  cin >> n;
 
- int a = 1;
+ bool flag = true;
 
- for (int i=1 ; i<=n ; i++){
-  cout << a << " ";
-  a = a * 2;
- } 
+ for (int i=2 ; i<=n/2 ; i++){
+  if (n%i == 0){
+   flag = false;
+   break;
+  }
+ }
+
+ if (n == 1){
+  cout << "Neither prime nor composite.";
+ }
+ else if (flag == true){
+  cout << "Prime number.";
+ }
+ else {
+  cout << "Composite number.";
+ }
 
  return 0;
 }
