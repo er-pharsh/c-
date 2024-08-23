bbbca54dce7ac55b0e34a93605973de5cc7f5b07
Harsh <er.pharsh@gmail.com>
2024-08-23 22:47:44 +0530

    Add new code

--- loop.cpp
+++ loop.cpp
@@ -68,28 +68,84 @@ int main(){
 
  // Number is prime or composite
 
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // bool flag = true;
+
+ // for (int i=2 ; i<=n/2 ; i++){
+ //  if (n%i == 0){
+ //   flag = false;
+ //   break;
+ //  }
+ // }
+
+ // if (n == 1){
+ //  cout << "Neither prime nor composite.";
+ // }
+ // else if (flag == true){
+ //  cout << "Prime number.";
+ // }
+ // else {
+ //  cout << "Composite number.";
+ // }
+
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // for (int i=1 ; i<=n ; i++){
+ //  if (i%2 == 0){
+ //   continue;
+ //  }
+ //  cout << i << " ";
+ // }
+
+ // WHILE LOOP
+
+ // int i = 1;
+ // while(i<=10){
+ //  cout << i << " ";
+ //  i++;
+ // }
+
+ // DO WHILE LOOP
+
+ // int i = 5;
+ // do{
+ //  cout << i << " ";
+ //  i--;
+ // }while(i>1);
+
+ // NUMBER OF THE INTEGER
+
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // int count = 0;
+
+ // while(n != 0){
+ //  n = n/10;
+ //  count++;
+ // }
+ // cout << "Number of digit is " << count;
+
+ // SUM OF INTEGER DIGITS
+
  int n;
  cout << "Enter the number :- ";
  cin >> n;
+ 
+ int sum = 0;
 
- bool flag = true;
-
- for (int i=2 ; i<=n/2 ; i++){
-  if (n%i == 0){
-   flag = false;
-   break;
-  }
+ for (int i=n ; n!=0 ; n/=10){
+  int l = n%10;
+  sum += l;
  }
 
- if (n == 1){
-  cout << "Neither prime nor composite.";
- }
- else if (flag == true){
-  cout << "Prime number.";
- }
- else {
-  cout << "Composite number.";
- }
+ cout << "Sum of the integer is " << sum;
 
  return 0;
 }
