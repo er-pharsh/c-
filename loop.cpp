d9af57dcee6368415f08ed3af42a244fc1b8f250
Harsh <er.pharsh@gmail.com>
2024-08-23 21:14:11 +0530

    Add new code

--- loop.cpp
+++ loop.cpp
@@ -10,13 +10,39 @@ int main(){
  //  cout << i << " ";
  // }
 
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // for (int i=1 ; i<=10 ; i++){
+ //  cout << n << " * " << i << " = " << n*i << endl;  
+ // }
+
+ // AP a = a + (n-1)d     3 6 9 12 15 18........
+
+ // int n;
+ // cout << "Enter the number :- ";
+ // cin >> n;
+
+ // int a = 1;
+
+ // for (int i=1 ; i<=n ; i++){
+ //  cout << a << " ";
+ //  a = a + 2;
+ // }
+
+ // GP 1 2 4 8 16 32.......
+
  int n;
  cout << "Enter the number :- ";
  cin >> n;
 
- for (int i=1 ; i<=10 ; i++){
-  cout << n << " * " << i << " = " << n*i << endl;  
- }
+ int a = 1;
+
+ for (int i=1 ; i<=n ; i++){
+  cout << a << " ";
+  a = a * 2;
+ } 
 
  return 0;
 }
