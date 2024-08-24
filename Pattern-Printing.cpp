1df7bf6dfd164c507615520d74c49bf5fc106023
Harsh <er.pharsh@gmail.com>
2024-08-24 21:43:27 +0530

    Add new code

--- Pattern-Printing.cpp
+++ Pattern-Printing.cpp
@@ -37,11 +37,82 @@ int main(){
 
  // int n = 6;
  // for (int i=n ; i>=1 ; i--){
- //  for (int z=(i-1) ; z<(n-1) ; z++){
+ //  for (int z=(i) ; z<=(n-1) ; z++){
  //   cout << "  ";
  //  }
  //  for (int j=1 ; j<=i ; j++){
  //   cout << "* ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=5 ; j++){
+ //   cout << i << " ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=5 ; j++){
+ //   cout << char(i+64) << " ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=i ; j++){
+ //   if (i%2 != 0){
+ //    cout << j << " "; 
+ //   }
+ //   else {
+ //    cout << char(j+64) << " ";
+ //   }
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=5 ; i>=1 ; i--){
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << j << " ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << 5 + 1 - i << " ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  // int d = 0;
+ //  for (int j=1 ; j<=i ; j++){
+ //   // cout << j+d << " ";
+ //   cout << 2*j-1 << " ";
+ //   // d++;
+ //  }
+ //  cout << endl;
+ // }
+
+ // int n = 1;
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << n << " ";
+ //   n++;
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int  j=1 ; j<=i ; j++){
+ //   if ((i+j)%2 == 0){
+ //    cout << "1" << " ";
+ //   }
+ //   else {
+ //    cout << "0 ";
+ //   }
  //  }
  //  cout << endl;
  // }
