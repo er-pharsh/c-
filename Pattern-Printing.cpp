c362ad0ec0b3e7424d74e5fc5875172562d2d888
Harsh <er.pharsh@gmail.com>
2024-08-24 15:00:23 +0530

    Add new code

---
+++ Pattern-Printing.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=5 ; j++){
+ //   cout << "* ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=1 ; i<=5 ; i++){
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << "* ";
+ //  }
+ //  cout << endl;
+ // }
+
+ // for (int i=5 ; i>=1 ; i--){
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << "* ";
+ //  }
+ //  cout << endl;
+ // }
+
+//  int n = 5;
+//  for (int i=1 ; i<=n ; i++){
+//   for (int j=(n-i) ; j>=1 ; j--){
+//    cout << "  ";
+//   }
+//   for (int z=1 ; z<=i ; z++){
+//    cout << "* ";
+//   }
+//   cout << endl;
+//  }
+
+ // int n = 6;
+ // for (int i=n ; i>=1 ; i--){
+ //  for (int z=(i-1) ; z<(n-1) ; z++){
+ //   cout << "  ";
+ //  }
+ //  for (int j=1 ; j<=i ; j++){
+ //   cout << "* ";
+ //  }
+ //  cout << endl;
+ // }
+
+ return 0;
+}
