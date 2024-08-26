1e6707077a0a9cc5de56888a5cffe460311a0338
Harsh <er.pharsh@gmail.com>
2024-08-26 12:41:13 +0530

    Add new recursion programs

--- recursion.cpp
+++ recursion.cpp
@@ -9,18 +9,64 @@ using namespace std;
 //  print(x-1);
 // }
 
-void print(int x){
- if (x == 0){
-  return;
- }
- print(x-1);
- cout << x << endl;
+// void print(int x){
+//  if (x == 0){
+//   return;
+//  }
+//  print(x-1);
+//  cout << x << endl;
  
-}
+// }
+
+// int sum(int x){
+//  if (x == 0){
+//   return 0;
+//  }
+//  return x + sum(x-1);
+// }
+
+// int fect(int x){
+//  if (x == 0){
+//   return 1;
+//  }
+//  return x*fect(x-1);
+// }
+
+// int power(int a , int b){
+//  if (b == 0){
+//   return 1;
+//  }
+//  return a *  power(a , b-1);
+// }
+
+// int fibo(int x){
+//  if (x == 1 || x == 2){
+//   return 1;
+//  }
+//  else if (x == 0){
+//   return 0;
+//  }
+//  return fibo(x-2) + fibo(x-1);
+// }
 
 int main(){
  int n;
- cout << "Enter the number :- ";
+ cout << "Enter the number n :- ";
  cin >> n;
- print(n);
+
+ // int m;
+ // cout << "Enter the number m :- ";
+ // cin >> m;
+
+ // print(n);
+
+ // cout << sum(n);
+
+ // cout << fect(n);
+
+ // cout << power(n,m);
+
+ // cout << fibo(n);
+
+ return 0;
 }
