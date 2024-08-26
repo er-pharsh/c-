ab7ecd4f7f16ca12bd315f78bfde8e8aa614ae97
Harsh <er.pharsh@gmail.com>
2024-08-26 20:16:52 +0530

    Add new programs

---
+++ array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+ // int n;
+ // cout << "Enter the size of array :- ";
+ // cin >> n;
+ // int arr[n];
+ // for (int i=0 ; i<=n-1 ; i++){
+ //  cin >> arr[i];
+ // }
+ // int sum = 0;
+ // for (int j=0 ; j<=n-1 ; j++){
+ //  sum += arr[j];
+ // }
+ // cout << "Sum of the array is :- " << sum;
+
+ int arr[] = {2,2,3,4,5,1};
+ int n = sizeof(arr)/4;
+
+ int mx = arr[0];
+ for (int i=1 ; i<n ; i++){
+  if (mx < arr[i]){
+   mx = arr[i];
+  }
+ }
+
+ cout << "Max value is :- " << mx;
+
+
+ return 0;
+}
