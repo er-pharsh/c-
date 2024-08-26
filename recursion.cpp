04846b2142a0481bb0ddf1641910b93a38abed19
Harsh <er.pharsh@gmail.com>
2024-08-26 14:24:07 +0530

    Add new programs

--- recursion.cpp
+++ recursion.cpp
@@ -49,14 +49,22 @@ using namespace std;
 //  return fibo(x-2) + fibo(x-1);
 // }
 
+int powerLog(int a , int b){
+ int c = (b/2);
+ if (c == 0){
+  return 1;
+ }
+ return a * powerLog(a , c) * powerLog(a , c);
+}
+
 int main(){
  int n;
  cout << "Enter the number n :- ";
  cin >> n;
 
- // int m;
- // cout << "Enter the number m :- ";
- // cin >> m;
+ int m;
+ cout << "Enter the number m :- ";
+ cin >> m;
 
  // print(n);
 
@@ -67,6 +75,8 @@ int main(){
  // cout << power(n,m);
 
  // cout << fibo(n);
+
+ cout << powerLog(n , m);
 
  return 0;
 }
